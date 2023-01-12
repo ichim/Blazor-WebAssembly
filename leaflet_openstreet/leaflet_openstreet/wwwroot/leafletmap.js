f8d9e63fec940d3db021273bde7a988c9c2154ef
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2023-01-12 21:15:21 +0200

    Leaflet Map
    Using Leaflet Map into Blazor Page. Working with Leaflet Map and GeoJSON file

---
+++ leaflet_openstreet/leaflet_openstreet/wwwroot/leafletmap.js
@@ -0,0 +1,27 @@
+﻿export function load_map(raw) {
+    console.log(JSON.parse(String(raw)));
+    let map = L.map('map').setView({ lon: 26.097133, lat: 44.446165  }, 16);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
+    var geojson_layer = L.geoJSON().addTo(map);
+    var geojson_data = JSON.parse(String(raw));
+    for (var geojson_item of geojson_data) {
+        geojson_layer.addData(geojson_item);
+        var marker = new L.marker(
+            [geojson_item.geometry.coordinates[1],
+            geojson_item.geometry.coordinates[0]],
+            { opacity: 0.01 }
+        );
+        marker.bindTooltip(geojson_item.properties.name,
+            {
+                permanent: true,
+                className: "my-label",
+                offset: [0, 0]
+            }
+        );
+        marker.addTo(map);
+    }
+
+
+
+    return "";
+}
