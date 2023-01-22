5b5b2c20bcdce32850e792e047812140fd7b5f31
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2023-01-22 15:55:06 +0200

    Blazor and spreadsheet
    Create Spreadsheet Application into Blazor page, using Jspreadsheet API

---
+++ spreadsheets/spreadsheets/wwwroot/jspreadsheet.js
@@ -0,0 +1,18 @@
+﻿export function load_spreadsheet() {
+
+
+
+    jspreadsheet.setLicense('[your license key code]');
+    jspreadsheet(document.getElementById('spreadsheet'), {
+        toolbar: true,
+        worksheets: [
+            {
+                minDimensions: [10, 10],
+                worksheetName: "Calculus"
+            },
+            { minDimensions: [10, 20] }
+        ],
+        tabs:true
+    });
+    return "";
+}
